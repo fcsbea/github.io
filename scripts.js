f8bfe24c4d500366a405560076c857ba384de66b
fcsbea <freeman.eric@fcsbea.com>
2025-04-27 13:13:09 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,5 +1,36 @@
-<body>
-    <h1>Welcome to Freeman Consulting Solutions LLC</h1>
-    <p>Your trusted partner in business consulting.</p>
-    <script src="scripts.js"></script>
-</body>
+body {
+    background-image: url('background.jpg');
+    background-size: cover;
+    font-family: 'Roboto', sans-serif;
+    color: #333;
+    margin: 0;
+    padding: 0;
+}
+
+header {
+    text-align: center;
+    padding: 50px 0;
+    background: rgba(0, 0, 0, 0.5);
+    color: #fff;
+}
+
+.services {
+    display: flex;
+    justify-content: space-around;
+    padding: 50px 0;
+}
+
+.service {
+    background: rgba(255, 255, 255, 0.8);
+    padding: 20px;
+    border-radius: 10px;
+    width: 30%;
+    text-align: center;
+}
+
+footer {
+    text-align: center;
+    padding: 20px 0;
+    background: rgba(0, 0, 0, 0.5);
+    color: #fff;
+}
